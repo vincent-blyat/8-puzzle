4085fdc05f622b80e283e1e06302afe1b5e331ac
vincent-blyat <a.v.musa2017@gmail.com>
2020-11-16 10:36:44 +0800

    Updates for Helper.h

--- src/Helper.h
+++ src/Helper.h
@@ -6,4 +6,20 @@
 
 #include<stdio.h>
 
+extern struct node;
+
 #endif
+
+/////////////////////////
+// Function Prototypes //
+/////////////////////////
+
+extern Node* newNode(int d,int p);
+
+extern int peek(Node** head);
+
+extern void pop(Node** head);
+
+extern void push(Node** head, int d, int p);
+
+extern int isEmpty(Node** head);
